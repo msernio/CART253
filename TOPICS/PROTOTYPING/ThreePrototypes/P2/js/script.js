f5e09a89fe5b685b0466f049b1a2ba080328e146
msernio <s4095318@student.rmit.edu.au>
2026-09-23 22:17:37 -0400

    Added a bunch of shapes and overlapped them with each other to create a simple abstract

--- TOPICS/PROTOTYPING/ThreePrototypes/P2/js/script.js
+++ TOPICS/PROTOTYPING/ThreePrototypes/P2/js/script.js
@@ -12,13 +12,92 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(750, 750);
 
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    // call the function - run the code
+    background(0,0,161);
+
+    drawRow1();
+    drawRow2();
+    drawRow3();
+    drawRect1();
+    drawRect2();
+    drawRect3();
+    drawRect4();
 
 }
+
+function drawRect1() {
+    push();
+    fill(0,0,161);
+    stroke(0,0,161);
+    rect(0, 380, 480, 100);
+    pop();
+
+}
+
+function drawRect2() {
+    push();
+    fill(0,0,161);
+    stroke(0,0,161);
+    rect(490, 0, 150, 500);
+    pop();
+
+}
+
+function drawRect3() {
+    push();
+    fill(0,0,161);
+    stroke(0,0,161);
+    rect(0, 15, 250, 100);
+    pop();
+
+}
+
+function drawRect4() {
+    push();
+    fill(0,0,161);
+    stroke(0,0,161);
+    rect(380, 500, 120, 250);
+    pop();
+
+}
+
+
+ // function to draw roof
+function drawRow1() {
+    push();
+    fill(255, 144, 9);
+    stroke(255, 144, 9);
+    ellipse(width/2, 120, 200, 200);
+    ellipse(135, 120, 200, 200);
+    ellipse(620, 120, 200, 200);
+    pop();
+
+}
+
+function drawRow2() {
+    push();
+    fill(255, 144, 9);
+    stroke(255, 144, 9);
+    ellipse(width/2, height/2, 200, 200);
+    ellipse(135, height/2, 200, 200);
+    ellipse(620, height/2, 200, 200);
+    pop();
+
+}
+
+function drawRow3() {
+    push();
+    fill(255, 144, 9);
+    stroke(255, 144, 9);
+    ellipse(width/2, 625, 200, 200);
+    ellipse(135, 625, 200, 200);
+    ellipse(620, 625, 200, 200);
+    pop();
+
+}
+
